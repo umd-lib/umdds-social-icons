f002162f627e14390e9d27504804c9f8a67d30aa
Jeremy M Gottwig <jeremy.m.gottwig@gmail.com>
2025-03-03 07:58:00 -0500

    LIBWEB-6288. Wiring plugin up to the new templates.
    https://umd-dit.atlassian.net/browse/LIBWEB-6288

--- src/Plugin/Block/UMDDSSocialIcons.php
+++ src/Plugin/Block/UMDDSSocialIcons.php
@@ -27,6 +27,13 @@ class UMDDSSocialIcons extends BlockBase {
     $blockConfig = $this->getConfiguration();
     $block_heading = !empty($blockConfig['block_heading']) ? $blockConfig['block_heading'] : t('Stay Connected');
     $icons = [];
+    if (!empty($blockConfig['bluesky_url'])) {
+      $render_arr = [
+        '#theme' => 'umdds_social_icons_bluesky',
+        '#url' => $blockConfig['bluesky_url'],
+      ];
+      $icons[] = \Drupal::service('renderer')->render($render_arr);
+    }
     if (!empty($blockConfig['facebook_url'])) {
       $render_arr = [
         '#theme' => 'umdds_social_icons_facebook',
@@ -38,6 +45,41 @@ class UMDDSSocialIcons extends BlockBase {
       $render_arr = [
         '#theme' => 'umdds_social_icons_instagram',
         '#url' => $blockConfig['instagram_url'],
+      ];
+      $icons[] = \Drupal::service('renderer')->render($render_arr);
+    }
+    if (!empty($blockConfig['mastodon_url'])) {
+      $render_arr = [
+        '#theme' => 'umdds_social_icons_mastodon',
+        '#url' => $blockConfig['mastodon_url'],
+      ];
+      $icons[] = \Drupal::service('renderer')->render($render_arr);
+    }
+    if (!empty($blockConfig['pinterest_url'])) {
+      $render_arr = [
+        '#theme' => 'umdds_social_icons_pinterest',
+        '#url' => $blockConfig['pinterest_url'],
+      ];
+      $icons[] = \Drupal::service('renderer')->render($render_arr);
+    }
+    if (!empty($blockConfig['threads_url'])) {
+      $render_arr = [
+        '#theme' => 'umdds_social_icons_threads',
+        '#url' => $blockConfig['threads_url'],
+      ];
+      $icons[] = \Drupal::service('renderer')->render($render_arr);
+    }
+    if (!empty($blockConfig['tumblr_url'])) {
+      $render_arr = [
+        '#theme' => 'umdds_social_icons_tumblr',
+        '#url' => $blockConfig['tumblr_url'],
+      ];
+      $icons[] = \Drupal::service('renderer')->render($render_arr);
+    }
+    if (!empty($blockConfig['wordpress_url'])) {
+      $render_arr = [
+        '#theme' => 'umdds_social_icons_wordpress',
+        '#url' => $blockConfig['wordpress_url'],
       ];
       $icons[] = \Drupal::service('renderer')->render($render_arr);
     }
@@ -81,6 +123,12 @@ class UMDDSSocialIcons extends BlockBase {
       '#title' => t('Core Services'),
       '#open' => TRUE,
     ];
+    $form['core_services']['bluesky_url'] = [
+      '#type' => 'textfield',
+      '#title' => t('BlueSky URL'),
+      '#description' => t('Not yet implemented.'),
+      '#default_value' =>  !empty($config['bluesky_url']) ? $config['bluesky_url'] : null,
+    ];
     $form['core_services']['facebook_url'] = [
       '#type' => 'textfield',
       '#title' => t('Facebook URL'),
@@ -89,6 +137,21 @@ class UMDDSSocialIcons extends BlockBase {
     $form['core_services']['instagram_url'] = [
       '#type' => 'textfield',
       '#title' => t('Instagram URL'),
+      '#default_value' =>  !empty($config['instagram_url']) ? $config['instagram_url'] : null,
+    ];
+    $form['core_services']['mastodon_url'] = [
+      '#type' => 'textfield',
+      '#title' => t('Mastodon URL'),
+      '#default_value' =>  !empty($config['mastodon_url']) ? $config['mastodon_url'] : null,
+    ];
+    $form['core_services']['pinterest_url'] = [
+      '#type' => 'textfield',
+      '#title' => t('Pinterest URL'),
+      '#default_value' =>  !empty($config['pinterest_url']) ? $config['pinterest_url'] : null,
+    ];
+    $form['core_services']['wordpress_url'] = [
+      '#type' => 'textfield',
+      '#title' => t('Wordpress URL'),
       '#default_value' =>  !empty($config['instagram_url']) ? $config['instagram_url'] : null,
     ];
     $form['core_services']['youtube_url'] = [
@@ -102,29 +165,22 @@ class UMDDSSocialIcons extends BlockBase {
       '#title' => t('Alternative Services'),
       '#open' => FALSE,
     ];
-    $form['alt_services']['bluesky_url'] = [
-      '#type' => 'textfield',
-      '#title' => t('BlueSky URL'),
-      '#description' => t('Not yet implemented.'),
-      '#default_value' =>  !empty($config['bluesky_url']) ? $config['bluesky_url'] : null,
-    ];
-    $form['alt_services']['mastodon_url'] = [
-      '#type' => 'textfield',
-      '#title' => t('Mastodon URL'),
-      '#description' => t('Not yet implemented.'),
-      '#default_value' =>  !empty($config['mastodon_url']) ? $config['mastodon_url'] : null,
-    ];
     $form['alt_services']['threads_url'] = [
       '#type' => 'textfield',
       '#title' => t('Threads URL'),
       '#description' => t('Not yet implemented.'),
       '#default_value' =>  !empty($config['threads_url']) ? $config['threads_url'] : null,
     ];
+    $form['alt_services']['tumblr_url'] = [
+      '#type' => 'textfield',
+      '#title' => t('Tumblr URL'),
+      '#default_value' =>  !empty($config['tumblr_url']) ? $config['tumblr_url'] : null,
+    ];
     $form['alt_services']['muskweb_url'] = [
       '#type' => 'textfield',
       '#title' => t('X URL'),
       '#default_value' =>  !empty($config['muskweb_url']) ? $config['muskweb_url'] : null,
-      '#description' => t('Consider BlueSky or Mastadon as alternatives to X, which offer similar functionality.'),
+      '#description' => t('Note that BlueSky or Mastadon offer similar functionality.'),
     ];
     return $form;
   }
@@ -135,12 +191,15 @@ class UMDDSSocialIcons extends BlockBase {
   public function blockSubmit($form, FormStateInterface $form_state) {
     $vals = $form_state->getValues();
     $this->setConfigurationValue('block_heading', $form_state->getValue('block_heading'));
+    $this->setConfigurationValue('pinterest_url', $vals['core_services']['pinterest_url']);
+    $this->setConfigurationValue('wordpress_url', $vals['core_services']['wordpress_url']);
     $this->setConfigurationValue('youtube_url', $vals['core_services']['youtube_url']);
     $this->setConfigurationValue('facebook_url', $vals['core_services']['facebook_url']);
     $this->setConfigurationValue('instagram_url', $vals['core_services']['instagram_url']);
-    $this->setConfigurationValue('muskweb_url', $vals['alt_services']['muskweb_url']);
-    $this->setConfigurationValue('bluesky_url', $vals['alt_services']['bluesky_url']);
-    $this->setConfigurationValue('mastodon_url', $vals['alt_services']['mastodon_url']);
+    $this->setConfigurationValue('bluesky_url', $vals['core_services']['bluesky_url']);
+    $this->setConfigurationValue('mastodon_url', $vals['core_services']['mastodon_url']);
+    $this->setConfigurationValue('tumblr_url', $vals['alt_services']['tumblr_url']);
     $this->setConfigurationValue('threads_url', $vals['alt_services']['threads_url']);
+    $this->setConfigurationValue('muskweb_url', $vals['alt_services']['muskweb_url']);
   }
 }
